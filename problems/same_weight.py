1dd5734ac9339a88871fe1f8b38967eabcf4d86f
Sean Stephenson <sms@pixar.com>
2015-04-17 14:42:47 -0700

    stub out problems

---
+++ problems/same_weight.py
@@ -0,0 +1,12 @@
+#!/usr/bin/env python
+
+# find the closes int with same weight.
+# the weight of a non negative int x is number of bits that are 
+# set to 1 in the binary representation. for ex 92 in base 2 is
+# (1011100)2, the weight of 92 is 4.
+
+def main():
+    print 'main'
+
+if __name__ == '__main__':
+    main()
