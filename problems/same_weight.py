b1594adc6088831ae9f545a3274436ccbbd96c75
Sean Stephenson <sms@pixar.com>
2015-04-17 14:45:47 -0700

    add definition

--- problems/same_weight.py
+++ problems/same_weight.py
@@ -1,9 +1,14 @@
 #!/usr/bin/env python
 
 # find the closes int with same weight.
+#
 # the weight of a non negative int x is number of bits that are 
 # set to 1 in the binary representation. for ex 92 in base 2 is
 # (1011100)2, the weight of 92 is 4.
+#
+# write a program which takes a non negative int x and return y,
+# such that y is not equal to x, and y has same weight as x, and 
+# the difference between y and x is as small as possible
 
 def main():
     print 'main'
